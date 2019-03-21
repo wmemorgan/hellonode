478dd1a4baf367de1120933bec413412850b16cf
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-21 20:56:23 +0800

    Update server message

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello there!!!\n");
+  response.end("<h2>Welcome to My Super Duper Server</h2>\n<p>Made with CI/CD magic</p>");
 });
 
 // listen on localhost:8000
